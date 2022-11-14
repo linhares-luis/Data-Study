bcd17fa162e1bd16adb103598f4c4ab157d90224
Luis Linhares <79722718+linhares-luis@users.noreply.github.com>
2022-11-14 15:31:33 -0300

    Add files via upload

--- 8-weeks-SQL-challenge/Case-Study#2/SQL/A_PizzaMetrics.sql
+++ 8-weeks-SQL-challenge/Case-Study#2/SQL/A_PizzaMetrics.sql
@@ -26,27 +26,37 @@ FROM
 	FROM customer_orders GROUP BY pizza_id,extras,exclusions) T
 
 -- 3. How many successful orders were delivered by each runner?
-SELECT runner_id, COUNT(order_id) order_runner
+SELECT runner_id, COUNT(order_id) delivered_order
 FROM runner_orders
 WHERE cancellation IS NULL
 GROUP BY runner_id;
 
 -- 4. How many of each type of pizza was delivered?
-SELECT pizza_id, COUNT(pizza_id) as delivered_pizzas 
-FROM customer_orders c 
-	JOIN runner_orders r
-	ON c.order_id =r.order_id
-WHERE cancellation IS NULL
-GROUP BY pizza_id;
+WITH pizzas_delivered (pizza_id, delivered_pizzas) as
+(
+	SELECT pizza_id, COUNT(pizza_id) as delivered_pizzas 
+	FROM customer_orders c 
+		JOIN runner_orders r
+		ON c.order_id =r.order_id
+	WHERE cancellation IS NULL
+	GROUP BY pizza_id
+)
+SELECT p.pizza_name, d.delivered_pizzas
+FROM pizzas_delivered d
+	JOIN pizza_names p
+	ON d.pizza_id = p.pizza_id
 
 -- 5. How many Vegetarian and Meatlovers were ordered by each customer?
-SELECT c.customer_id, n.pizza_name, c.ordered_customer
-FROM ( SELECT customer_id, pizza_id,  COUNT(pizza_id) ordered_customer
-	   FROM customer_orders 
-	   GROUP BY customer_id, pizza_id) as c
-	JOIN pizza_names n
-	ON c.pizza_id = n.pizza_id
+SELECT c.customer_id, 
+	CAST(p.pizza_name AS VARCHAR(10)) pizza_name, 
+	COUNT(c.pizza_id) amt_ordered
+FROM customer_orders c
+	JOIN pizza_names p
+	ON c.pizza_id = p.pizza_id
+GROUP BY c.customer_id, CAST(p.pizza_name AS VARCHAR(10))
 ORDER BY c.customer_id
+
+
 
 -- 6. What was the maximum number of pizzas delivered in a single order?
 SELECT TOP 1 C.order_id, COUNT(c.order_id) pizza_in_order
@@ -61,8 +71,7 @@ ORDER BY COUNT(c.order_id) DESC
 WITH delivered_order (customer_id, order_id, extras, exclusions, changed) AS (
 SELECT c.customer_id, c.order_id, c.extras, c.exclusions, 
 changed = CASE
-	WHEN c.extras IS NOT NULL THEN 1
-	WHEN c.exclusions IS NOT NULL THEN 1
+	WHEN (c.extras IS NOT NULL) or (c.exclusions IS NOT NULL) THEN 1
 	ELSE 0
 	END
 FROM customer_orders c 
@@ -70,7 +79,7 @@ FROM customer_orders c
 	ON c.order_id = r.order_id 
 WHERE cancellation IS NULL
 )
-SELECT customer_id, SUM(changed) pizz_w_change
+SELECT customer_id, SUM(changed) pizza_w_change, (COUNT(customer_id) - SUM(changed)) no_change_orders
 FROM delivered_order
 GROUP BY customer_id;
 go
@@ -89,6 +98,8 @@ WHERE cancellation IS NULL
 SELECT SUM(changed) pizz_both_change
 FROM delivered_order
 
+
+
 -- 9. What was the total volume of pizzas ordered for each hour of the day?
 SELECT DATENAME(hour, order_time) hour_day, COUNT(*) amt_pizzas 
 FROM customer_orders
@@ -98,4 +109,3 @@ GROUP BY DATENAME(hour, order_time)
 SELECT DATENAME(WEEKDAY, order_time) week_day, COUNT(*) amt_pizzas 
 FROM customer_orders
 GROUP BY DATENAME(WEEKDAY, order_time)
-order by 
